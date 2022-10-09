2a4b49951763f699ffff272377fad62b358163c8
Achem-Habib <achemnomaer57@gmail.com>
2022-10-09 18:36:50 +0600

    some changes

---
+++ src/Components/Navbar.js
@@ -0,0 +1,124 @@
+import { Disclosure, Transition } from "@headlessui/react";
+import {} from "@heroicons/react/24/outline";
+import { Fragment } from "react";
+
+const navigation = [
+  { name: "About", href: "#", current: true },
+  { name: "Skill", href: "#", current: false },
+  { name: "Work", href: "#", current: false },
+  { name: "Contact", href: "#", current: false },
+];
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
+export default function Navbar() {
+  return (
+    <Disclosure as="nav" className=" fixed w-full top-0 ">
+      {({ open }) => (
+        <>
+          <div className="mx-auto px-2 sm:px-6 lg:px-8 bg-gray-800">
+            <div className="relative flex h-16 items-center justify-between">
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                {/* Mobile menu button*/}
+                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="currentColor"
+                      viewBox="0 0 24 24"
+                      strokeWidth="1.5"
+                      stroke="#06b6d4"
+                      className="w-6 h-6 fill-cyan-500"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M6 18L18 6M6 6l12 12"
+                      />
+                    </svg>
+                  ) : (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      className="w-6 h-6 fill-cyan-500"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  )}
+                </Disclosure.Button>
+              </div>
+              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                <div className="flex flex-shrink-0 items-center">
+                  <p className="block  lg:hidden text-4xl text-cyan-500 font-bold">
+                    AHN
+                  </p>
+                  <p className="hidden  lg:block text-4xl text-cyan-500 font-bold">
+                    AHN
+                  </p>
+                </div>
+                <div className="hidden sm:ml-6 sm:block">
+                  <div className="flex space-x-4">
+                    {navigation.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className={classNames(
+                          item.current
+                            ? "bg-gray-900 text-white"
+                            : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                          "px-3 py-2 rounded-md text-sm font-medium"
+                        )}
+                        aria-current={item.current ? "page" : undefined}
+                      >
+                        {item.name}
+                      </a>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <Transition
+            as={Fragment}
+            enter="transition ease-linear duration-300"
+            enterFrom="transform opacity-0 -translate-y-0"
+            enterTo="transform opacity-100 translate-y-0"
+            leave="transition ease-in duration-300"
+            leaveFrom="transform opacity-100 translate-y-0"
+            leaveTo="transform opacity-0 -translate-y-0"
+          >
+            <Disclosure.Panel className="sm:hidden z-30 bg-gray-800">
+              <div className="space-y-1 px-2 pt-2 pb-3">
+                {navigation.map((item) => (
+                  <Disclosure.Button
+                    key={item.name}
+                    as="a"
+                    href={item.href}
+                    className={classNames(
+                      item.current
+                        ? "bg-gray-900 text-white"
+                        : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                      "block px-3 py-2 rounded-md text-base font-medium"
+                    )}
+                    aria-current={item.current ? "page" : undefined}
+                  >
+                    {item.name}
+                  </Disclosure.Button>
+                ))}
+              </div>
+            </Disclosure.Panel>
+          </Transition>
+        </>
+      )}
+    </Disclosure>
+  );
+}
